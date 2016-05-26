f6a48815b523c81cacda7721996f8ea6596945ac
Trevor Little <trevor@learnersguild.org>
2016-05-26 11:25:58 -0400

    version bump

--- HISTORY.md
+++ HISTORY.md
@@ -1,4 +1,7 @@
 # History
 
+#### 0.2.0
+Adding channel delete API
+
 #### 0.1.0
 Initial version
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'learnersguild:rocketchat-lg-api-extensions',
-  version: '0.1.1',
+  version: '0.2.0',
   summary: 'Custom API extensions for Rocket.Chat within Learners Guild.',
   git: 'https://github.com/LearnersGuild/rocketchat-lg-api-extensions'
 })
