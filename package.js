397f4c11dd4e35975322a9a2d5c1e441f95481e0
jeffreywescott <jeffrey@learnersguild.org>
2016-05-26 10:28:38 -0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'learnersguild:rocketchat-lg-api-extensions',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'Custom API extensions for Rocket.Chat within Learners Guild.',
   git: 'https://github.com/LearnersGuild/rocketchat-lg-api-extensions'
 })
