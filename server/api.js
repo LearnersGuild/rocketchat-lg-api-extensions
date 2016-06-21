9ea88e8a9b49a575ae57772cfa23dfa72ba04e2b
jeffreywescott <jeffrey@learnersguild.org>
2016-06-21 15:15:46 -0700

    add API to create a channel with topic and members

--- server/api.js
+++ server/api.js
@@ -24,6 +24,48 @@ function successResponse(data, statusCode = 200) {
   }
 }
 
+const validRoomName = new RegExp('^[0-9a-zA-Z-_.]+$', 'i')
+
+Api.addRoute('rooms', {authRequired: true}, {
+  post() {
+    try {
+      const {
+        userId,
+        bodyParams: {
+          name,
+          topic,
+          members,
+        },
+      } = this
+      if (!RocketChat.authz.hasPermission(userId, 'create-c')) {
+        throw new Meteor.Error('insufficient-permissions', 'you do not have permission to do this')
+      }
+      if (!validRoomName.test(name)) {
+        throw new Meteor.Error('invalid-room-name', `${name} is not a valid room name`)
+      }
+      /* eslint-disable prefer-arrow-callback */
+      const result = Meteor.runAsUser(userId, function () {
+        const {rid} = Meteor.call('createChannel', name, members)
+        RocketChat.saveRoomTopic(rid, topic)
+        return {
+          status: 'success',
+          room: {
+            rid,
+            name,
+            topic,
+            members,
+          },
+        }
+      })
+      return successResponse({result})
+    } catch (error) {
+      console.error(error.stack)
+      RavenLogger.log(error)
+      return errorResponse(error)
+    }
+  },
+})
+
 Api.addRoute('rooms/:name/send', {authRequired: true}, {
   post() {
     try {
